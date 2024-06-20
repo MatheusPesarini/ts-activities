90f457d237d61bcdd4a57f6c4bc301e6e499c2e5
MatheusPato <mrogeriopesarini@gmail.com>
2024-06-20 01:18:12 -0300

    Indo dormir

---
+++ parte_1_ReactRouterDom_Vite/ReactRouterDom/src/components/About/index.tsx
@@ -0,0 +1,9 @@
+﻿import './style.css';
+
+export const About = () => {
+    return (
+        <div>
+            <h1>About</h1>
+        </div>
+    );
+};
---
+++ parte_1_ReactRouterDom_Vite/ReactRouterDom/src/components/Menu/index.tsx
@@ -0,0 +1,13 @@
+﻿import './style.css';
+import { Link } from 'react-router-dom';
+
+export const Menu = () => {
+    return (
+        <nav className={"menu"}>
+            <ul>
+                <li><Link to={"/"}>Home</Link></li>
+                <li><Link to={"/about"}>Home</Link></li>
+            </ul>
+        </nav>
+    );
+};
