bb759ea1ff292e0c920aa118851d4ebc071c1a22
MatheusPato <mrogeriopesarini@gmail.com>
2024-06-22 01:48:41 -0300

    Progresso...

---
+++ parte_2_JSExpreRegulares/aula1.js
@@ -0,0 +1,32 @@
+﻿// g - global: procura por todas as ocorrências
+// i - insensitive: ignora maiúsculas e minúsculas
+// () - grupos
+// | - ou
+
+
+const { texto } = require('./base');
+
+const regExp1 = /João/gi;
+const found1 = regExp1.exec(texto);
+
+console.log(texto)
+console.log(texto.match(regExp1));
+
+console.log(texto.replace(/João/gi, 'Felipe'));
+console.log(texto.replace(/(João)/gi, '--$1--'));
+console.log(texto.replace(/(João|cão)/gi, function (input){
+    return input.toUpperCase();
+}));
+
+
+// * - 0 ou n (opcional)
+// + - 1 ou n (obrigatório)
+// ? - 0 ou 1 (opcional)
+
+const regExp2 = /Jo+ão/gi;
+console.log(texto.match(regExp2));
+
+// / para caso seja usado caracteres especiais
+
+const regExp3 = /\./g;
+console.log(texto.match(regExp3));
---
+++ parte_2_JSExpreRegulares/base.js
@@ -0,0 +1,3 @@
+﻿const texto = `João trouxe um cão para casa. João estava passeando com cão. Jooooooooooão`;
+
+module.exports = { texto };
